582d2e5e8e5e35f409bb0507af1c494347dea7ec
ranguiquan <ranguiquan@gmail.com>
2023-04-23 23:01:07 +0800

    feat: Refactor Docker configuration for "example-dag" DAG.
    - Add "Hello World" example script in a new Python file
    - Update DAG name, image name, command, network mode, and mount source path to reflect real data in docker-compose.yaml and example.py
    - Change permission for /var/run/docker.sock to 666 in docker-compose.yaml

--- dags/example.py
+++ dags/example.py
@@ -14,23 +14,27 @@ default_args = {
     "retry_delay": timedelta(minutes=5),
 }
 with DAG(
-    "docker_dag",
+    "example-dag",
     default_args=default_args,
     schedule_interval="*/2 * * * *",
     catchup=False,
 ) as dag:
     t1 = DockerOperator(
         task_id="docker",
+        # TODO: image name should be changed to the real image name
         image="airflow-worker-python",
         api_version="auto",
         auto_remove=True,
         # https://stackoverflow.com/questions/52458528/executing-python-and-bash-commands-using-dockeroperator
-        command='bash -c "cd scripts && python test.py"',
+        # TODO: Enter the project file and run it
+        command='bash -c "cd scripts && python example.py"',
         docker_url="unix://var/run/docker.sock",
-        network_mode="fin_pip_airflow-net",
+        # TODO: Check network name
+        network_mode="airflow-compose_net",
         mounts=[
             Mount(
-                source="/home/ranguiquan/workspace/project/fin_pip/scripts",
+                # TODO: Change it to full path of scripts
+                source="/home/ranguiquan/workspace/airflow-compose/scripts",
                 target="/workspace/scripts",
                 type="bind",
             )
---
+++ scripts/example.py
@@ -0,0 +1,6 @@
+import datetime
+import os
+
+print("Hello World")
+print(datetime.datetime.now())
+print(os.getcwd())
